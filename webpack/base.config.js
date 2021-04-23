e20c3d615a4d9aa4fdeacdb3d4b0effa806e594d
Faruk Garic <faruk@heftig.co>
2021-04-23 04:20:28 +0200

    Add local development configuration

---
+++ webpack/base.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    context: path.resolve(__dirname, '../src'),
+    entry: {
+        app: './js/index.js'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "../style.css",
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader' },
+                    {
+                        /* for ~slick-carousel/slick/slick-theme.sass */
+                        loader: 'resolve-url-loader' },
+                    {
+                        /* for resolve-url-loader:
+                            source maps must be enabled on any preceding loader */
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    }
+                ]
+            }
+        ]
+    }
+}
